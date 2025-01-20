b634ca21659fe189ce494535c1cebf0c57d05b08
Lucas Leme Cardoso Ferreira <lucasleme09@gmail.com>
2025-01-20 12:56:56 -0300

    Teste

--- infra/main.tf
+++ infra/main.tf
@@ -4,59 +4,55 @@ provider "aws" {
 
 # Função Lambda para processamento de frames
 resource "aws_lambda_function" "register_user" {
-  function_name = "frame_process_function" # Nome fixo da função Lambda
+  function_name = "frame_process_function"
 
-  handler = "process.lambda_handler" # Atualizado para o handler da função de procesamento
-  runtime = "python3.8"
-  role    = aws_iam_role.lambda_process_role.arn
-
-  # Caminho para o código da função Lambda.
+  handler          = "process.lambda_handler"
+  runtime          = "python3.8"
+  role             = aws_iam_role.lambda_process_role.arn
   filename         = "../lambda/process/process_lambda_function.zip"
   source_code_hash = filebase64sha256("../lambda/process/process_lambda_function.zip")
 
-  # Configuração de camadas
   layers = [
     "arn:aws:lambda:us-east-1:145266761615:layer:ffmpeg-lambda-layer:1"
   ]
-
 }
 
 # Role para Lambda
 resource "aws_iam_role" "lambda_process_role" {
-  name = "lambda_execution_role" # Nome fixo da role
+  name = "lambda_execution_role"
 
   assume_role_policy = jsonencode({
-    Version = "2012-10-17"
+    Version = "2012-10-17",
     Statement = [
       {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
+        Action = "sts:AssumeRole",
+        Effect = "Allow",
         Principal = {
           Service = "lambda.amazonaws.com"
         }
-      },
+      }
     ]
   })
 }
 
-# Política de Permissões de envio de email para Lambda
+# Política de Permissões
 resource "aws_iam_policy" "lambda_email_policy" {
   name        = "lambda_email_policy"
   description = "Permissões necessárias para a Lambda enviar emails"
 
   policy = jsonencode({
-    "Version" : "2012-10-17",
-    "Statement" : [
+    Version : "2012-10-17",
+    Statement : [
       {
-        "Effect" : "Allow",
-        "Action" : [
+        Effect : "Allow",
+        Action : [
           "s3:GetObject",
           "s3:DeleteObject",
           "s3:PutObject",
           "s3:PutObjectAcl",
           "lambda:GetLayerVersion"
         ],
-        "Resource" : "*"
+        Resource : "*"
       }
     ]
   })
